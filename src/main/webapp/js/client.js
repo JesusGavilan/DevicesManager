76d4f4fa993fc85dda2a6bf2aeb98d3978c63b69
gerardcl <gerardcl@gmail.com>
2013-11-01 23:29:01 +0100

    testing client, first get attempt

---
+++ src/main/webapp/js/client.js
@@ -0,0 +1,215 @@
+(function($){
+
+	// Backbone.Model.prototype.idAttribute = '_id';
+
+	var Device = Backbone.Model.extend({
+		// url: '/devices/routers.json',
+		defaults: function(){
+			return {	
+				id: ' ',
+				ip: ' ',
+				name: ' ',
+				description: ' '
+			};			
+		}
+	});
+
+	var DevicesList = Backbone.Collection.extend({
+		model: Device,
+		url: 'http://localhost:8080/device-manager-api/webapi/devices/routers/RT-1'
+		// url: '/devices/routers.json',
+
+//		,initialize: function () {
+//			this.bind("reset", function (model, options) {
+//				console.log("Inside event");
+//				console.log(model);
+//
+//			});
+//		}
+
+
+
+	});
+	var numDevices = 0;
+	var devices = new DevicesList();
+
+	var DeviceView = Backbone.View.extend({
+		model: new Device(),
+		tagName: 'div',
+		events: {
+			'click .name': 'updateName',
+			'click .ip': 'updateIp',
+			'click .description': 'updateDesc',
+			'click .delete': 'deletee',
+			'click .details': 'details',
+			'blur .name': 'close',
+			'blur .ip': 'close',
+			'blur .description': 'close',
+			'keypress .name': 'onEnterUpdate',
+			'keypress .ip': 'onEnterUpdate',
+			'keypress .description': 'onEnterUpdate'
+		},
+		initialize: function(){
+			this.template = _.template($('#device-template').html()); // using
+			// underscore
+			// template
+		},
+		updateName: function(ev){
+			ev.preventDefault();
+			this.$('.name').attr('contenteditable',true).focus();
+		},
+		updateIp: function(ev){
+			ev.preventDefault();
+			this.$('.ip').attr('contenteditable',true).focus();
+		},
+		updateDesc: function(ev){
+			ev.preventDefault();
+			this.$('.description').attr('contenteditable',true).focus();
+		},
+		details: function(ev){
+
+		},
+		close: function(ev){
+			var self = this;
+			var name = this.$('.name').text();
+			var ip = this.$('.ip').text();
+			var description = this.$('.description').text();
+			var id = this.$('.id').text();
+			// this.model.set('name', name);
+			// this.model.set('ip', ip);
+			// this.model.set('description', description);
+			this.model.save({ id: id ,name: name ,ip: ip, description: description }, {
+				success: function(){ console.log('Successfully updated device '+ self.model.id); },
+				error: function(){ console.log("Failed to update device with id = "+self.model.id); }
+			});
+			this.$('.name').removeAttr('contenteditable');
+			this.$('.ip').removeAttr('contenteditable');
+			this.$('.description').removeAttr('contenteditable');
+		},
+		onEnterUpdate: function(ev){
+			var self = this;
+			if(ev.keyCode==13){
+				this.close();
+				_.delay(function(){ self.$('.name').blur()},100);
+				_.delay(function(){ self.$('.ip').blur()},100);
+				_.delay(function(){ self.$('.description').blur()},100);
+			}
+		},
+		deletee: function(ev){
+			ev.preventDefault();
+			var self = this;
+			this.model.destroy({
+				success: function() { devices.remove(this.model); console.log("Delete succesfully done!"); },
+				error: function() { console.log("Failed to remove device with id = "+self.model.idAttribute); }
+			});
+		},
+		render: function(){
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+		}
+	});
+
+	var DevicesView = Backbone.View.extend({
+		model: devices,
+		el: $('#devices-container'),
+		initialize: function(){
+			var self = this;
+			this.model.on('add', this.render, this);
+			this.model.on('remove', this.render,this);
+			// get all devices (Backbone.sync powah!!!)
+			this.model.fetch({
+				success: function(response,xhr) {
+					console.log("Inside success");
+					console.log(response+' '+xhr);
+					console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Inside success");
+					var models = response.models[0];
+					$.each(models,function(index,value){
+						console.log("---->>>>>>>"+ index+" : "+value);
+					});
+					console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Inside success");
+
+					
+					console.log("UUUUUUUInside Parse");
+
+					// keys
+					var keys = response.COLUMNS;
+					console.log("keys = "+keys);
+					// values
+					var values = response.DATA;
+
+					// Parse the response and construct models
+					for (var i = 0, length = values.length; i < length; i++) {
+
+						var currentValues = values[i];
+						var deviceObject = {};
+						for (var j = 0, valuesLength = currentValues.length; j < valuesLength; j++) {
+							deviceObject[keys[j]] = currentValues[j];
+						}
+
+						// push the model object
+						this.push(deviceObject);	
+					}
+
+					console.log(this.toJSON());
+
+					// return models
+					return this.models;
+
+				},
+				error: function (errorResponse) {
+					console.log(errorResponse)
+				}
+			});
+
+		},					
+		render: function(){
+			var self = this;
+			self.$el.html('');
+			_.each(this.model.toArray(),function(device,i){
+				self.$el.append((new DeviceView({model: device})).render().$el);
+			});
+			return this;
+		}
+
+	});
+
+	var Router = Backbone.Router.extend({
+		routes: {
+			'': 'index',
+			'devices/:id': 'show'
+		},
+		index: function(){
+			// enable Add Device
+			console.log("INDEX");
+			// disable show device
+		},
+		show: function(id){
+			// disable add device
+			console.log("SHOW");
+			// enable show device
+		}
+	});
+
+	$(document).ready(function(){
+		$('#add-device').submit(function(ev){
+			var the_id= "RT-"+ ++numDevices;
+			var device = new Device({id:the_id,name:$('#device-name').val(),ip:$('#device-ip').val(),description:$('#device-description').val()});
+			devices.add(device);
+			// var device2 = new Device({id:'RT-2',name:'Router
+			// 2',ip:'192.168.1.12',description:'second router'});
+			// var device3 = new Device({id:'RT-3',name:'Router
+			// 3',ip:'192.168.1.13',description:'third router'});
+			// console.log(device.get('description'));
+			console.log(devices.toJSON());
+//			device.save({id:device.get('id'),name:$('#device-name').val(),ip:$('#device-ip').val(),description:$('#device-description').val()},{
+//			succes: function(){ consol.log("successfully saved device!");},
+//			error: function(){ console.log("error saving device!");}
+//			})
+			return false;	
+		});
+
+		var appView = new DevicesView();
+	});
+
+
+})(jQuery);
---
+++ src/main/webapp/js/client2.js
@@ -0,0 +1,145 @@
+(function ($) {
+    var Router = Backbone.Model.extend({
+        defaults:{
+        	 interfaces: "",
+             ports: "",
+             routingTable: ""
+        },
+        parse:function (response) {
+            console.log(response);
+            response.id = response.inventoryId;
+            return response;
+        }
+    });
+
+    var RouterCollection = Backbone.Collection.extend({
+        model:Router,
+        url:'http://localhost:8080/device-manager-api/webapi/devices/routers'
+    });
+
+    var RouterView = Backbone.View.extend({
+        tagName:"div",
+        className:"wrapper",
+        template:$("#device-template").html(),
+
+        render:function () {
+            var tmpl = _.template(this.template); //tmpl is a function that takes a JSON and returns html
+
+            this.$el.html(tmpl(this.model.toJSON())); //this.el is what we defined in tagName. use $el to get access to jQuery html() function
+            return this;
+        },
+
+//        events:{
+//            "click .delete":"deleteRouter"
+//        },
+
+//        deleteRouter:function () {
+//            console.log('Destroying router _id: ' + this.model.get("id"));
+//            //Delete model
+//            this.model.destroy({
+//                error:function (model, response) {
+//                    console.log("Failed destroying book");
+//                },
+//                success:function (model, response) {
+//                    console.log("Succeeded in destroying book");
+//                }
+//            });
+//
+//            //Delete view
+//            this.remove();
+//        }
+    });
+
+    var RouterCollectionView = Backbone.View.extend({
+        el:$("#routers"),
+
+        initialize:function () {
+            //this.collection = new Library(books);
+        	var self = this;
+            this.collection = new RouterCollection();
+            this.collection.fetch({
+            	success:function(response){
+            		console.log("Success fetchingg");
+                    self.render();
+                    self.collection.push
+
+            	},
+                error:function () {
+                    console.log(arguments);
+                }
+            });
+            this.render();
+
+            this.collection.on("add", this.renderRouter, this);
+//            this.collection.on("remove", this.removeBook, this);
+            this.collection.on("reset", this.render, this);
+        },
+
+        render:function () {
+            var that = this;
+    		console.log("Start rendering");
+
+            _.each(this.collection.models, function (item) {
+        		console.log("Rendering item");
+                that.renderRouter(item);
+            });
+        },
+
+//        events:{
+//            "click #add":"addBook"
+//        },
+//
+//        addBook:function (e) {
+//            e.preventDefault();
+//
+//            var formData = {};
+//
+//            $("#addBook div").children("input").each(function (i, el) {
+//                if ($(el).val() !== "") {
+//                    if (el.id === 'keywords') {
+//                        var keywordArray = $(el).val().split(',');
+//                        var keywordObjects = [];
+//                        for (var j = 0; j < keywordArray.length; j++) {
+//                            keywordObjects[j] = {"keyword":keywordArray[j]};
+//                        }
+//                        formData[el.id] = keywordObjects;
+//                    } else if (el.id === 'releaseDate'){
+//                        formData[el.id] = $('#releaseDate').datepicker("getDate").getTime();
+//                    } else {
+//                        formData[el.id] = $(el).val();
+//                    }
+//                }
+//            });
+//
+//            books.push(formData);
+//
+//            //this.collection.add(new Book(formData));
+//            this.collection.create(formData);
+//        },
+
+//        removeBook:function (removedBook) {
+//            var removedBookData = removedBook.attributes;
+//
+//            _.each(removedBookData, function (val, key) {
+//                if (removedBookData[key] === removedBook.defaults[key]) {
+//                    delete removedBookData[key];
+//                }
+//            });
+//
+//            _.each(books, function (book) {
+//                if (_.isEqual(book, removedBookData)) {
+//                    books.splice(_.indexOf(books, book), 1);
+//                }
+//            });
+//        },
+
+        renderRouter:function (item) {
+            var routerView = new RouterView({
+                model:item
+            });
+            this.$el.append(routerView.render().el);
+        }
+    });
+
+    var routerCollectionView = new RouterCollectionView();
+})(jQuery);
